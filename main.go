cf33b5613675aa7ecfa3ef9388ae645ba6615d5e
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-07 14:02:20 +0800

    simple http server
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
-// HelloWorld for hello world
-func HelloWorld() string {
-	return "Hello World, golang workshop!"
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s, I love %s!", HelloWorld(), r.URL.Path[1:])
+	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	var port string
-	flag.StringVar(&port, "port", "8080", "server port")
-	flag.StringVar(&port, "p", "8080", "server port")
-	if p, ok := os.LookupEnv("PORT"); ok {
-		port = p
-	}
-	flag.Parse()
 	http.HandleFunc("/", handler)
-	log.Println("http server run on " + port + " port")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Println("http server run on 8080 port")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
