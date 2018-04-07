aa9888e5b4e039c4adf20c4bd6fde684f6d8696d
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-04-07 10:41:14 +0800

    feat: add simple http server

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 // HelloWorld for hello world
 func HelloWorld() string {
-	return "Hello World, drone workshop!"
+	return "Hello World, golang workshop!"
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s, I love %s!", HelloWorld(), r.URL.Path[1:])
 }
 
 func main() {
-	fmt.Println(HelloWorld())
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	http.HandleFunc("/", handler)
+	log.Println("http server run on " + port + " port")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
