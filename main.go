675494ec91f5e8825d0001d287538d6612626321
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-01-10 00:48:58 +0800

    update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func HelloWorld() string {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Got http request. time: %v", time.Now())
-	fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "I love %s!, %s", r.URL.Path[1:], HelloWorld())
 }
 
 func pinger(port string) error {
